8a692085896b0721ad5da81e2913ac53d1c5b594
gacenga_ <122737849+gacenga@users.noreply.github.com>
2024-09-11 22:16:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function playMusic(song) {
+    const audio = document.getElementById('audio');
+    const audioSource = document.getElementById('audioSource');
+    audioSource.src = song;
+    audio.load();
+    audio.play();
+}
 
